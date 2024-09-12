5007e529753327bf55a80153a96181fd7e6714f2
Altereo <evanlee868@gmail.com>
2024-09-12 14:54:40 +1000

    Implemented proper account managment popover (moved logout button there) and added dummy profile picture upload button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule, RouterLink, RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
@@ -12,7 +14,7 @@ import { ChatApiService, User } from './chat-api.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, CommonModule, HttpClientModule],
+  imports: [RouterOutlet, RouterModule, CommonModule, HttpClientModule, NgbPopoverModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -20,7 +22,21 @@ export class AppComponent implements OnInit {
   user: Observable<User>;
   isLoggedIn: boolean = false;
 
+  fileInput: string = "";
+  profileImage: string = "";
+
   title = '3813ICT-Chat';
+
+
+  onFileChange(event: any) {
+    let files = event.target.files as FileList;
+
+    if (files.length > 0) {
+      let _file = URL.createObjectURL(files[0]);
+      this.profileImage = _file;
+      this.fileInput = "";
+    }
+  }
 
 
   triggerLogout() {
